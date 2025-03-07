35d0601151103f05d774998d4854bb71918651c8
Krrina <106991553+Krrina@users.noreply.github.com>
2025-03-07 07:55:47 +0000

    dfdf

---
+++ MyProject/Classes/User.php
@@ -0,0 +1,32 @@
+<?php
+namespace MyProject\Classes;
+
+class User{
+    public $name;
+    public $login;
+    private $password;
+
+    public function __construct($name, $login, $password) {
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password; 
+    }
+
+    public function showinfo(){
+        echo "<H2> Создан пользователь с параметрами: </H2>";
+        echo "<p> Имя: $name </p>";
+        echo "<p> Логин: $login </p>";
+        echo "<p> Пароль: $password </p>"; 
+    }
+
+    public function getPassword() {
+            return $this->password;
+        }
+
+        public function setPassword($password) {
+         $this->password = $password;
+        }
+}
+
+
+?>
