a3311ba20a66e92a5635b0c9cd572ef63c3e53f2
Krrina <106991553+Krrina@users.noreply.github.com>
2025-04-14 11:01:02 +0000

    ддmb

--- lab3/patterns/mvc_use.php
+++ lab3/patterns/mvc_use.php
@@ -10,3 +10,32 @@ ini_set('display_errors', 1);
 
 $controller = new Controller('users.md');
 echo "<pre>" . $controller->render() . "</pre>";
+
+echo "<!DOCTYPE html>
+<html>
+<head>
+    <title>Список пользователей</title>
+    <style>
+
+        .diagram-btn {
+            display: inline-block;
+            margin-top: 20px;
+            padding: 10px 15px;
+            background-color: #4CAF50;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+            font-weight: bold;
+            transition: background-color 0.3s;
+        }
+        .diagram-btn:hover {
+            background-color: #E9967A;
+        }
+    </style>
+</head>
+<body>";
+
+// Кнопка для просмотра диаграммы классов
+echo "<a href='https://sun9-26.userapi.com/impg/UHpsJ9XXVkVeLt-yVvoCzS6f1oa-qj3B-4_JKA/YUCHp-9Ob_A.jpg?size=906x555&quality=95&sign=b3bfd5c1cb44d9772f7db16f2521ea43&type=album' target='_blank' class='diagram-btn'>Просмотреть диаграмму классов</a>";
+
+echo "</body></html>";
